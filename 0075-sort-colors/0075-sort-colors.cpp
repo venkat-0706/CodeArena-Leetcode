f51250edb9e4a6a75e8a4fe53a36ffe423d22b89
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-03-02 04:47:09 +0530

    Time: 0 ms (100%), Space: 11.5 MB (68.49%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> count = {{0, 0}, {1, 0}, {2, 0}};
-
-        for (int num : nums) {
-            count[num]++;
-        }
-
-        int idx = 0;
-        for (int color = 0; color < 3; color++) {
-            int freq = count[color];
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = color;
-                idx++;
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i] > nums[j]) {
+                    swap(nums[i],nums[j]);
+                }
             }
-        }        
+        }
     }
 };
