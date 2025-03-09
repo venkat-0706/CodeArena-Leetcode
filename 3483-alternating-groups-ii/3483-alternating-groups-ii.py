cc09da75cf2617e29e2c0bdb086b2221b0049f87
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-03-09 20:03:12 +0530

    Time: 747 ms (35.88%), Space: 21.1 MB (97.34%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.py
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -1,32 +1,30 @@
 class Solution:  
     def numberOfAlternatingGroups(self, colors, k):  
-        length = len(colors)  
-        result = 0  
-        alternating_elements_count = 1  
-        last_color = colors[0]  
+        n = len(colors)  
+        if n < k:  
+            return 0  
 
-        for index in range(1, length):  
-            if colors[index] == last_color:  
-                alternating_elements_count = 1  
-                last_color = colors[index]  
-                continue  
+        count = 0  
+        length = 1  
+        prev_color = colors[0]  
 
-            alternating_elements_count += 1  
+        for i in range(1, n):  
+            if colors[i] != prev_color:  
+                length += 1  
+                if length >= k:  
+                    count += 1  
+            else:  
+                length = 1  
+            prev_color = colors[i]  
 
-            if alternating_elements_count >= k:  
-                result += 1  
+        if k > 1:  
+            for i in range(min(k - 1, n)):  
+                if colors[i] != prev_color:  
+                    length += 1  
+                    if length >= k:  
+                        count += 1  
+                else:  
+                    break  
+                prev_color = colors[i]  
 
-            last_color = colors[index]  
-
-        for index in range(k - 1):  
-            if colors[index] == last_color:  
-                break  
-
-            alternating_elements_count += 1  
-
-            if alternating_elements_count >= k:  
-                result += 1  
-
-            last_color = colors[index]  
-
-        return result  
+        return count  
