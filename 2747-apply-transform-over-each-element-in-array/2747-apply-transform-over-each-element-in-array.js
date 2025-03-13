3226b32b887a2c69c87ddf70023e55c6faf8bca6
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-03-13 06:15:14 +0530

    Time: 42 ms (80.66%), Space: 53.4 MB (22.43%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -1,8 +1,7 @@
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
 var map = function(arr, fn) {
-    return arr.map(fn);
+  const transformedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    transformedArr[i] = fn(arr[i], i);
+  }
+  return transformedArr;
 };
