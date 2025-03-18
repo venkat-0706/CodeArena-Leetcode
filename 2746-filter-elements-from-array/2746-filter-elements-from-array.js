406bdc0c0a1209e51fedfcdc9239820f7766e811
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-03-19 05:15:34 +0530

    Time: 41 ms (81.04%), Space: 53.9 MB (13.22%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,6 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn)
-    
+    let fil=[]
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            fil.push(arr[i]);
+        }
+    }
+    return fil
 };
