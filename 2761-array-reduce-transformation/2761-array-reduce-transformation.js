71db7696bf59b96389f664cc4e0048e8453e1a81
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-03-21 01:39:12 +0530

    Time: 39 ms (95.32%), Space: 56.3 MB (6.36%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for(const elements of nums){
+        val = fn(val,elements);
+    }
+    return val;
+
+};
