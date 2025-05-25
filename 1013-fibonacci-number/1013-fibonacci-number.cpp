65777cea23da1da8e8af721f6388f3f5e78d95aa
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-05-25 09:48:48 +0530

    Time: 2 ms (75.89%), Space: 7.7 MB (87.58%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<0){
-            return 0;
-        }if(n==1){
-            return 1;
-        }else{
-            return fib(n-1)+fib(n-2);
+        int a=0,b=1,c;
+        for(int i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
         }
+        return a;
     }
 };
