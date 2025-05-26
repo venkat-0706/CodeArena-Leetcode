e08c1be7d068b2e987909192decb8a672638b4c0
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-05-26 08:44:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(long n) {
-        if(n==0){
+     if(n==0){
+        return false;
+     }   
+     while(n!=1){
+        if(n&1!=0){
             return false;
         }
-        return ((n&(n-1))==0);
+        n=n/2;
+     }
+     return true;
     }
 };
