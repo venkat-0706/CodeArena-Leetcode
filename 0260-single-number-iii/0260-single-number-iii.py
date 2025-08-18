df02fbada37b468443b56f693b2b51f1484494d6
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-08-18 08:26:07 +0530

    Time: 1 ms (79.7%), Space: 19.1 MB (62.69%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,13 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        n=len(nums)
         x=nums[0]
-        for i in range(1,n):
+        for i in range(1,len(nums)):
             x^=nums[i]
-        k = (x&(~(x-1)))
+        k=(x&(~(x-1)))
         res,ans=0,0
-        for i in range(n):
-            if(nums[i]&k!=0):
+        for i in range(len(nums)):
+            if nums[i]&k!=0:
                 res^=nums[i]
             else:
                 ans^=nums[i]
