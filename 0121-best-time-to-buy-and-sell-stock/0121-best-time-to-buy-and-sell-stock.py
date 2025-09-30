333ec1438504466c802a1f1e1dec8ee8157c34c4
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-09-30 08:27:19 +0530

    Time: 66 ms (73.51%), Space: 26.8 MB (94.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,11 +2,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy_price = prices[0]
         profit = 0
-
         for p in prices[1:]:
             if buy_price > p:
                 buy_price = p
-            
-            profit = max(profit, p - buy_price)
-        
+            profit = max(profit , p-buy_price)
         return profit
+        
