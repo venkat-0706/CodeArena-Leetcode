ba299aa9f2f63fdfd7e5864d990707d08a5d4762
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-12-25 07:20:10 +0530

    Time: 150 ms (77.31%), Space: 107.9 MB (47.16%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& christmasJoy, int gifts) {
+        sort(christmasJoy.begin(), christmasJoy.end(), greater<int>());
+        long long totalJoy = 0;
+        for (int turn = 0; turn < gifts; ++turn) {
+            int currentJoy = christmasJoy[turn] - turn;
+            if (currentJoy <= 0)
+                break;
+            totalJoy += currentJoy;
+        }
+        
+        return totalJoy;
+    }
+};
