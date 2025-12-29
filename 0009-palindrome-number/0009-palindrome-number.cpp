b2228fb4a74ad093b3fbd68c4a80cf7e6984d0d7
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-12-30 05:17:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    bool isPal(const string &s, int start, int end) {
-        if (start >= end){
-            return true;
-        } 
-        return (s[start] == s[end]) && isPal(s, start + 1, end - 1);
-    }
-
     bool isPalindrome(int x) {
-        if (x < 0){
+        long long int rev =0 , n=x;
+        if(x<0){
             return false;
-        }  
-        string s = to_string(x);
-        return isPal(s, 0, s.size() - 1);
+        }
+        while(x!=0){
+            rev *=  10;
+            rev += (x%10);
+            x  /=  10;
+        }
+        if(n==rev) return 1;
+        else return 0;
     }
 };
